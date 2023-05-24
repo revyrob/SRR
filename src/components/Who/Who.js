e3b7f1113299ffdc2c91331b98af8ea90a9b5475
revyrob <kayle.robson@gmail.com>
2023-05-23 23:48:56 -0700

    added who section

--- src/components/Who/Who.js
+++ src/components/Who/Who.js
@@ -4,7 +4,35 @@ function Who() {
   return (
     <section>
       <h1>Who is Proposing the Project</h1>
-      <h2>West High Yield (WHY) Resources Ltd. Open Pit Mine</h2>
+      <h3>West High Yield (WHY) Resources Ltd. Open Pit Mine</h3>
+      <div>
+        <h2>Who</h2>
+        <p>
+          West High Yield (WHY) Resources Ltd. is a junior explorer’s management
+          team let by Frank Marasco Jr. W.H.Y. Resources went public in 2006
+          after carrying out initial exploration activities at Midnight. Within
+          those years the owner and the executives of the WHY have been
+          penalized for their "magical" stock price event in October 2018
+          (https://www.asc.ca/News-and-Publications/News-Releases/2019/01/West-High-Yield-Resources-settles-with-the-ASC).
+          There has been questions about the ability of the company from their
+          poor presentation at the town hall meeting in Rossland.{" "}
+        </p>
+      </div>
+
+      <div>
+        <h2>What</h2>
+        <p>
+          Open pit mine that West High Yield (WHY) Resources Ltd. is proposing
+          to open in the Record Ridge area, on the unceded territory of the
+          Sinixt and other First Nations, near Rossland, Paterson and Sheep
+          Creek, BC.
+        </p>
+      </div>
+
+      <div>
+        <h2>Where</h2>
+        <img src="" alt="map of the opening mining proposal"></img>
+      </div>
     </section>
   );
 }
