fb3909aebb39226723f39c1329e378cb1720bab4
revyrob <kayle.robson@gmail.com>
2023-06-06 22:51:53 -0700

    added letter page

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -1,7 +1,292 @@
 import React from "react";
 
 function Letter() {
-  return <div>Letter</div>;
+  return (
+    <section className="p-8 md:p-12">
+      <div>
+        Your Name<br></br>
+        Your Address<br></br>
+        Date<br></br>
+      </div>
+      <br></br>
+      <div>
+        Name of Receiver<br></br>
+        Address of Receiver<br></br>
+        Email of Receiver<br></br>
+      </div>
+      <br></br>
+      <div>
+        To Name of Receiver,<br></br> <br></br>
+        <p>
+          As a local community member I am extremely concerned with the open pit
+          mine that West High Yield (WHY) Resources Ltd. is proposing to open in
+          the Record Ridge area, on the unceded territory of the Sinixt and
+          other First Nations, near Rossland, Paterson and Sheep Creek, BC. The
+          proposed mine could have significant negative impacts on the quality
+          of life, safety, health, economy, and well-being of all the residents.
+          In addition, the proposed mine will threaten an already fragile alpine
+          and grassland ecosystems and watershed.{" "}
+        </p>
+        <h4>Water</h4>
+        <p>
+          Having clean water is a right that all Canadians have. Many properties
+          around that watershed access surface or well water. With the proposed
+          mine location, there is an increased risk that runoff from the mine
+          could contaminate that watershed, either immediately or in the future.
+          This could result in homeowners bearing the costs of finding an
+          alternative way to get clean water. In addition, water is already
+          scarce in the Rossland area during the summer months. If the proposed
+          mine requires the use of any water, this will add significant pressure
+          to an already taxed system. Along with risking the drinking water of
+          residents, the proposed mine could also destroy fish habitat and the
+          water supply for the wildlife in the area. Under the Water
+          Sustainability Act, everyone needs a water license. How will the
+          watershed be protected, monitored, and sustained? What measures will
+          the province put in place to ensure the current and future protection
+          of that watershed?{" "}
+        </p>{" "}
+        <h4>Dust</h4>
+        <p>
+          The amount of dust that will be created from the blasting, crushing,
+          loading, and hauling activity will be significant. The local residents
+          living nearest the mine site, such as those families in Paterson, will
+          be constantly exposed to dust and airborne chemicals in the dust. How
+          will dust be mitigated at the mine site? How will the dust be
+          monitored for harmful contaminants? How will the province ensure local
+          residents have their homes, vehicles, and yards regularly cleaned from
+          excess dust from mining activity?
+        </p>
+        <h4>Asbestos and Other Airborne Chemicals</h4>
+        <p>
+          Within the ore, there are proven traces of asbestos. With trucks
+          driving through the center of town, estimated at every 7-10 minutes, 7
+          days per week, exposure to asbestos, or other chemicals in the ore
+          dust, is possible. Any exposure to asbestos is a serious health risk.
+          The tests that have been done, although they might be according to the
+          minimum regulations, are not enough to ensure that exposure to
+          asbestos is not a threat to our communities. How will the government
+          evaluate the contradicting reports of asbestos at the mine site? How
+          will the government monitor potential asbestos contamination at the
+          mine site and in the transport trucks traveling through our
+          communities? How will dust be monitored for such chemicals?
+        </p>{" "}
+        <h4>Noise Pollution</h4>
+        <p>
+          The noise from weekly blasting and daily crushing and hauling on a
+          mine site that is 7.5 kilometers from the community of Rossland and
+          only 2 kilometers from the community of Paterson will have an
+          extremely negative impact on the quality of life for all residents.
+          Moreover, the proposed mine site is only 200 meters from our epic
+          trail, Seven Summits, which is traversed by locals and tourists daily.
+          This noise will affect the patrons of the trail significantly. In
+          addition, the noise pollution from the heavy truck traffic through
+          Rossland, Paterson, and Trail will negatively impact all residents.
+          What will the province do to mitigate this noise in our communities?
+          How can the mine site be altered to buffer sound? Is it possible to
+          reduce hours of operation, including hauling, to be consistent with a
+          typical work schedule, thus permitting some regard for residents?
+        </p>{" "}
+        <h4>Transportation of Ore</h4>
+        <p>
+          With the proposed 80 ore-truck trips a day throughout Paterson and
+          Rossland, I am specifically concerned about road infrastructure and
+          the safety of citizens. The town of Rossland will carry the burden of
+          heavy traffic throughout town, affecting businesses, residents, and
+          homeowners. If the trucks are rerouted through Paterson, that
+          community will be negatively impacted. It is a farming community with
+          tractors and equipment sharing the road. How will the province ensure
+          the safety of community members, mitigate the negative impact of
+          increased heavy traffic on tourism, and reduce potential noise from
+          the big trucks?{" "}
+        </p>
+        <p>
+          In addition, the dust generated by the ore trucks hauling material on
+          the gravel road of cascade highway and through communities has the
+          potential to greatly affect air quality. It could also create a
+          blanket of dust on residents' homes, community sidewalks, and
+          storefronts. As queried earlier, how will the province ensure dust
+          from the ore trucks is mitigated? And how will the province ensure
+          that the homes and communities are kept clean?{" "}
+        </p>
+        <h4>Economic Impacts</h4>
+        <p>
+          Rossland has been named one of the best small mountain towns in Canada
+          to live in, according to Map Quest Travel, and has worked hard to
+          establish itself as a travel and tourist destination.. Rossland
+          accommodation revenue from tourism reached a record in 2022, where
+          “Total accommodation revenue was $9.5 million, a 40 percent increase
+          over the best year prior to the pandemic in 2019.” (Rossland News May
+          26, 2023). Rossland residents have invested significantly over the
+          last 20+ years in the city center and surrounding trails, parks, and
+          recreation areas to become a four-season outdoor recreation
+          destination. Many shops and services, and economic activity in
+          general, are focused on catering to this market. Many of the residents
+          of Rossland choose to live here because of the quality of life that
+          the area offers (ie, close access to wild nature, peace and quiet,
+          clean water, calm streets, world-class winter and summer recreation
+          possibilities, including trails that are tourist attractions).{" "}
+        </p>
+        <p>
+          Rossland is a popular tourist destination for mountain biking and all
+          types of skiing. In 2012, Rossland was named the “Mountain Biking
+          Capital of Canada'' by Explore Magazine. Tourism in Rossland in 2011,
+          recorded that out-of-town mountain bikers spent a total of $589,000,
+          which significantly contributed to the community’s revenues and added
+          to provincial revenues (Tourism BC, 2013). Since that time, mountain
+          biking revenues have continued to climb and are a major contributing
+          factor to the local economy and community.
+        </p>
+        <p>
+          The proposed mine would have a detrimental impact on access to and use
+          of the biking trails, and ski touring in the area of Record Ridge. In
+          particular, the Seven Summits Trail would be destroyed given its close
+          proximity to the proposed mine. It is uncertain how the growth of the
+          mine will further impact trail riding. WHY representatives have
+          proposed closing the Seven Summits trail during blasting or moving the
+          trail. It is unclear how all potential trail users would be notified
+          of the blasting, which could result in major safety concerns for local
+          or visiting patrons of the trail. Moving or closing the trail could
+          jeopardize the trail’s status and impact its value. Just having the
+          mine pit within sight of the trail will take away from the experience.
+          Moreover, there are major pick-up and drop-off points for the mountain
+          biking trails along the Cascade Highway. In 2007, Seven Summits Trail
+          was named “Trail of the Year” by Bike Magazine. In 2011, Tourism BC
+          conducted research on mountain biking in Rossland. They determined
+          that Seven Summits was the most popular trail in the community, with
+          61% of respondents reporting that they visited the trail during their
+          stay in Rossland. The trail was one of the top five reasons for
+          Rossland's biking popularity. The respondents were non-local riders.
+          The study concluded that 90% of travelers to the area reported that
+          the single most important factor in their decision to visit Rossland
+          was mountain biking. The majority also reported that they would return
+          to bike, given their experience. At that time, Rossland was reported
+          to be exceeding consumers' expectations for the biking experience.
+          Since 2011, Rossland has invested significant time and resources into
+          the trail. Currently, Seven Summits Trail is the only mountain biking
+          trail in the Kootenays with the international Mountain Biking
+          Association (IMBA) “Epic Status.” Canada only has four trails of this
+          status. This draws tourism and recreation from across the globe. In
+          addition, the mine’s existence and location would have a seriously
+          detrimental effect on The Broken Goat, the annual trail race based in
+          Rossland on the Seven Summits Trail.
+        </p>
+        <p>
+          In summary, the mine could destroy a tremendous amount of the tourism
+          industry in Rossland and negatively impact associated businesses. It
+          could potentially reduce the lifestyle, mental health, and physical
+          health of the local residents.
+        </p>
+        <p>
+          In addition, research also shows that our property values could
+          decrease by as much as 30%, due to our proximity to the open pit mine.
+          This further impacts local communities and investors. In exchange, the
+          only benefit that the representatives from the mine could identify for
+          residents of Rossland, Warfield, Trail, Paterson, and Sheep Creek was
+          approximately 30 seasonal jobs. We would lose far more than that many
+          jobs from the destruction of the very environment that enables our
+          thriving tourism industry.
+        </p>{" "}
+        <p>
+          How can the government ensure that the recreational and tourism
+          backbone of our community is not negatively impacted by Record Ridge
+          Mine? How will the government compensate for lost revenues and job
+          loss in this sector? How will the government ensure the safety of all
+          trail users, local and non-local during blasting times? How will the
+          integrity and experience of the world-famous Seven Summits Trail be
+          maintained? Within three kilometers of the mine site, there is a Girl
+          Guides Canada camp, which has been an established camping site for our
+          children and children from around Canada for many years. This
+          recreation site is used by children all summer long for excursions,
+          learning wilderness safety skills, and general camping events. They
+          depend on the water on site for drinking, bathing, and cleaning. The
+          noise and dust pollution will take away from their recreational
+          experience. How will the province mitigate the mining effects on this
+          recreational component of our community?
+        </p>
+        <h4>Farm Land</h4>
+        <p>
+          Paterson and Sheep Creek areas are home to some of the only viable
+          farmland Rossland has access to. This land is not protected by the
+          ALR. This agricultural land is at risk of being negatively impacted by
+          noise, dust, and water pollution. How will the province ensure that
+          the impact of the mine will not affect agricultural production of this
+          land and mitigate the effects of dust, noise, and water pollution for
+          the cattle, horses, and poultry of these farms?
+        </p>
+        <h4>First Nations’ Land Rights and UNDRIP</h4>
+        <p>
+          This proposed mine directly impacts the Sinixt people and their future
+          generations. It is their land we call home and it is their right to
+          protect it. This is their traditional unceded territory and they need
+          to be meaningfully consulted. A representative of the Sinixt came to
+          the community open house in Rossland on the evening of May 17th to
+          express in very strong and heartfelt words that the band DOES NOT WANT
+          IT. How will the province ensure that these indigenous people’s voices
+          are considered?
+        </p>
+        <h4>Grassland, endangered species, and unique ecosystem</h4>
+        <p>
+          The grassland deserves further study and has culturally significant
+          flora and endangered grasslands. There is a SARA S1 listed plant –
+          Mountain Holly Fern (Threatened) – that has multiple documented
+          occurrences on Record Ridge and within the proposed mine site (data
+          freely available with the BC CDC). How will the province ensure that
+          the ecosystem at Record Ridge is protected?
+        </p>
+        <h4>Safety</h4>
+        <p>
+          The safety of people driving, hiking, and cycling along the Cascade
+          Highway has not been considered. “Highway” is a misnomer for this
+          gravel road, designed, as one man who attended the Open House said,
+          “for Model-T and horse-drawn wagon.” This road features narrow
+          segments, blind corners, and precipitous drop-offs. In addition,
+          increased truck traffic through the centre of town would further
+          endanger the safety of drivers and pedestrians alike.
+        </p>
+        <h4>2-year pilot application for a 20+ year mining project</h4>
+        <p>
+          WHY (West High Yield Resources) has limited their application and the
+          information provided to a 2-year pilot project with the extraction of
+          only 249,000 tons of ore, to avoid the threshold of a large-scale
+          environmental assessment. The proponents told the community that they
+          intend to expand the mine site with additional applications after two
+          years. The community would like to know, prior to any pilot, the full
+          scope of the project and the effect it could have in their lives and
+          future generations. The potential hidden social, environmental, and
+          economic costs of this mine in the short and long term should be
+          considered when planning this project that will affect the life of
+          “Beautiful British Columbia” citizens. How can the province balance
+          the right of the applicant to just a two-year mining proposal and
+          ensure the local communities receive informed consent for a
+          potentially longer-term and more invasive project?
+        </p>
+        <p>
+          WHY mining company has no track record of successful, clean, and
+          environmentally safe projects. The company did not represent
+          themselves in a professional manner during the open house in Rossland,
+          and have failed to demonstrate that they are a collaborative or
+          trustworthy company. How will the province ensure that WHY Resources
+          can be trusted to maintain the integrity of the proposed permit? What
+          assurances can be given to the communities affected?
+        </p>{" "}
+        <p>
+          The mine will need to put forward a reclamation security. The security
+          is assessed on a site-specific basis and reflects the outstanding
+          reclamation, environmental and closure obligations associated with the
+          site. Please consider when defining the security, the social,
+          environmental, and community economic impacts that the mine will
+          incur, not only the site impact. Extracted from WHY website: “We are
+          committed to a sustainable future and to improving the social,
+          economic, and environmental well-being of the community.”
+        </p>{" "}
+        <p>
+          Please make WHY honor this statement. Thank you for your review of my
+          concerns.
+        </p>{" "}
+        <br></br>Sincerely,
+      </div>
+    </section>
+  );
 }
 
 export default Letter;
--- src/pages/LetterPage.js
+++ src/pages/LetterPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
+import Letter from "../components/Letter/Letter";
 
 function LetterPage() {
   return (
     <>
       <Nav />
-
+      <Letter />
       <Footer />
     </>
   );
